55cca07a55c7af3adb8150e29809f05e32eba448
Anttu Suhonen <anttu.suhonen@gmail.com>
2024-06-09 08:54:46 +0000

    Redis address

--- tasks.py
+++ tasks.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from celery import shared_task
 from redis import Redis
 
-r = Redis(host="localhost", port=6379, db=0, decode_responses=True)
+REDIS_HOST = "redis-service"
+r = Redis(host=REDIS_HOST, port=6379, db=0, decode_responses=True)
 
 
 @shared_task
